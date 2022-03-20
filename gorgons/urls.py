bf9233571e80bad4424a56ee68e3386d9256749f
Matheus Passos <matheushtip@gmail.com>
2022-03-19 21:32:09 -0300

    feat: Add global templates folder

--- gorgons/urls.py
+++ gorgons/urls.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-
-def my_view(_):
-    return HttpResponse("Hello World!")
 
 
 urlpatterns = [
