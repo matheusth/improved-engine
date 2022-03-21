d61bc2e1bfaebf48483d505f63d306297b65916b
Matheus Passos <matheushtip@gmail.com>
2022-03-21 13:24:19 -0300

    fix: Return 404 when a non-existing category is requested

--- recipes/views.py
+++ recipes/views.py
@@ -1,23 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from recipes.models import Recipe
+from django.http import Http404
 
 
 # Create your views here.
 def home(request):
     recipes = Recipe.objects.filter(published=True).order_by("-id")
     return render(request, 'recipes/home.html', context={
-        'recipes': recipes
+        'recipes': recipes,
+
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        published=True
-    ).order_by("-id")
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id,
+                              published=True).order_by("-id"))
 
-    return render(request, 'recipes/home.html', context={
-        'recipes': recipes
+    return render(request, 'recipes/pages/category-view.html', context={
+        'recipes': recipes,
+        'category_name': recipes[0].category.name,
     })
 
 
