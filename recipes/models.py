bc3327aa7d231b7adc436246a625f14614ef0f81
Matheus Passos <matheushtip@gmail.com>
2022-03-21 10:46:19 -0300

    feat: Register Category and Recipe model to the django-admin app

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return str(self.name)
+
 
 # Create your models here.
 class Recipe(models.Model):
@@ -27,3 +30,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
