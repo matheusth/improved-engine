96275a6c23da4b860844b2b504d5724df1d738c1
Matheus Passos <matheushtip@gmail.com>
2022-03-20 21:58:37 -0300

    feat(html, css, views): Add recipe view to show recipe details

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
+
+
+def recipe(request, recipe_id):
+    return render(request, 'recipes/pages/recipe-view.html')
