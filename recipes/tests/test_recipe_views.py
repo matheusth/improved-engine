b8636c915c7968c851085b20d93b185cc8ba3919
Matheus Passos <matheushtip@gmail.com>
2022-03-21 19:58:29 -0300

    test: add tests for when recipes are not found

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -7,14 +7,36 @@ from recipes import views  # type: ignore
 
 
 class RecipeViewsTest(TestCase):
-    def test_recipe_home_view_function_is_correct(self):
+    def test_recipes_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
-    def test_recipe_category_view_function_is_correct(self):
+    def test_recipes_home_view_returns_status_200(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipes_home_view_loads_expected_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/home.html')
+
+    def test_recipes_home_view_shows_no_recipes_found(self):
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        self.assertIn('não encontradas.', content)
+        self.assertTemplateNotUsed(response, 'recipes/partials/recipe.html')
+
+    def test_recipes_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', args=(1,)))
         self.assertIs(view.func, views.category)
 
-    def test_recipe_detail_view_function_is_correct(self):
+    def test_recipes_category_view_returns_status_404_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:category', args=(1000,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipes_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', args=(1,)))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipes_recipe_view_returns_status_404_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:recipe', args=(1000,)))
+        self.assertEqual(response.status_code, 404)
