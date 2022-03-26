dd0339c7b5e7d41238d1437fbf16ab8329c49543
Matheus Passos <matheushtip@gmail.com>
2022-03-26 18:01:27 -0300

    test: Add test for the default false fields of the recipe model

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError  # type: ignore
-from parameterized import parameterized
+from parameterized import parameterized  # type: ignore
+from recipes.models import Recipe  # type: ignore
 from .test_recipe_base import RecipeTestBase  # type: ignore
 
 
@@ -18,3 +19,18 @@ class RecipeModelTest(RecipeTestBase):
         setattr(self.recipe, field, "A" * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def test_recipe_model_published_and_preparation_steps_is_html_default_to_false(self):
+        local_recipe = Recipe.objects.create(
+            category=self.make_category(),
+            author=self.make_author(username="matheushti"),
+            description="descricao",
+            servings=2,
+            servings_unit="porções",
+            preparation_time=25,
+            preparation_time_unit="minutos",
+            preparation_steps="lorem ipsum dolor sit ammet"
+        )
+
+        self.assertIs(local_recipe.published, False, msg="published didn't default to false.")
+        self.assertIs(local_recipe.published, False, msg="preparation_steps_is_html didn't default to false.")
